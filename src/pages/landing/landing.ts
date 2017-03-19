dd78f0a712ef4789e9f30acb444f1978cfad76fe
Kuldeep Harshwal <rajharshwal92@gmail.com>
2017-03-19 16:44:30 +0530

    add landing page.

--- src/pages/landing/landing.ts
+++ src/pages/landing/landing.ts
@@ -1,22 +1,75 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, Injectable } from '@angular/core';
+import { NavController, NavParams, Platform } from 'ionic-angular';
+import { Device, Splashscreen } from 'ionic-native';
+//import { Config } from '../../providers/config.ts';
+//import { MyServices } from "../../providers/my-services";
 
-/*
-  Generated class for the Landing page.
+// declare var require: Function;
+// const localforage: LocalForage = require('localforage');
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-landing',
-  templateUrl: 'landing.html'
+  templateUrl: 'landing.html',
+  providers:[]
 })
 export class LandingPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public platform: Platform) {
+    this.platform = platform;
+    // localforage.config({
+    //   name: 'MyApp'
+    // });
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LandingPage');
+    this.getConfigData();
   }
+
+  checkHomeScreen() {
+
+  }
+
+  getConfigData() {
+    this.platform.ready().then(() => {
+      setTimeout(() => {
+        if (Device.platform == "iOS" || Device.platform == "Android") {
+          Splashscreen.hide();
+        }
+      }, 500);
+    })
+
+
+
+
+    /*setTimeout(() => {
+
+      localforage.getItem('config').then((foragedata) => {
+        if (foragedata) {
+          // MyServices.setconfigdata(foragedata);
+          Config.data = foragedata;
+          // configreload.func();
+          this.checkHomeScreen();
+        }
+        else {
+          console.log('else');
+
+          this.myServices.getallfrontmenu().map(response => response.json()).subscribe(data => {
+            // MyServices.setconfigdata(data);
+            Config.data = data;
+            // configreload.func();
+            console.log(data);
+            localforage.setItem('config', data);
+            console.log('landing page');
+            this.checkHomeScreen();
+          }, err =>{
+            console.log(err);
+            // $state.go('access.offline')
+          })
+        }
+      })
+    }, 3500);*/
+  }
+
 
 }
