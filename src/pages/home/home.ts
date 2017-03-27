2864c81275887a1ddd4cbc0bf216ba1427862f5a
Abhijeet Rathore <abhijeetrathoreiitd@gmail.com>
2017-03-27 10:56:24 +0530

    no message

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component , ViewChild} from '@angular/core';
+import { NavController, NavParams,LoadingController ,Slides} from 'ionic-angular';
 
 import { API } from '../../providers/api';
 import { commonServices } from '../../providers/common-services';
@@ -13,6 +13,7 @@ import { ImageCategoryPage } from "../image-category/image-category";
 import { RssArticlePage } from "../rss-article/rss-article"
 import * as localforage from "localforage";
 import {RefreshData} from "../../providers/refresh.service";
+import {LightboxPage} from '../lightbox/lightbox'
 
 
 
@@ -24,17 +25,34 @@ import {RefreshData} from "../../providers/refresh.service";
 })
 export class HomePage {
   headerLogo: string;
+  @ViewChild(Slides) sliders: Slides;
   slides: any;
   slider1Loading: boolean = true;
   banners: any;
   RSS = [];
   categories = [];
   content=[];
+  loading:any;
+  phoneNumber=0;
   baseImageUrl="http://business.staging.appturemarket.com/uploads/";
+  public allDataPromise: any = [];
+  public allObservableData: any = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public api: API, public commonServices: commonServices, public rs: RefreshData) {
-
+  constructor(public loadingCtrl: LoadingController,public navCtrl: NavController, public navParams: NavParams, public api: API, public commonServices: commonServices, public rs: RefreshData) {
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    let headerPromise = this.api.getHeaderLogo();
+    this.allDataPromise.push(headerPromise);
+    let sliderPromise = this.api.getallsliders()
+    this.allDataPromise.push(sliderPromise);
+    let promotionsPromise = this.api.getAllPromotions()
+    this.allDataPromise.push(promotionsPromise);
+    //this.fetchRSSData();
+    let frontMenuPromise = this.api.getAllFrontMenu()
+    this.allDataPromise.push(frontMenuPromise);
   }
+
 
   ionViewDidLoad() {
     // console.log("Header in Home form service",this.commonServices.headerLogo);
@@ -48,18 +66,77 @@ export class HomePage {
     this.fetchRSSData();
     // this.sortRssLinks(this.commonServices.AllMenuData);
      this.getAppconfig();
-  }
-  ngOnInit() {
-    this.rs.refreshData().then(function(){
 
-    });
-  }
+   }
+
+
   refreshdata(){
+
     let home=this;
+
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    this.sliders.stopAutoplay();
     localforage.removeItem("allObservbledata").then(function(){
       console.log("refresh call");
       //home.RefreshDataFun.refreshData();
-      home.rs.refreshData();
+    //  home.rs.refreshData();
+      home.loading.present();
+      console.log("inside service");
+          home.commonServices.slides=[];
+         // home.commonServices.banners=[];
+           home.commonServices.AllMenuData=[];
+           home.commonServices.menuData=[];
+      console.log("data change");
+      Observable.forkJoin(home.allDataPromise).subscribe((resPromise) => {
+        console.log("inside forkable");
+        home.allObservableData = resPromise;
+        localforage.setItem("allObservbledata",resPromise);
+        console.log(home.allObservableData);
+        // console.log(this.allObservableData[0]);
+        home.allObservableData[0]
+            .map(item => {
+              if (item.title == 'Header Logo') {
+                console.log(item);
+                home.commonServices.headerLogo = 'http://business.staging.appturemarket.com/uploads/header-logo/' + item.image;
+              }
+            });
+        // console.log(this.allObservableData[1]);
+        // console.log(this.allObservableData[2]);
+        home.commonServices.slides = home.allObservableData[1];
+        localforage.setItem("slides",home.commonServices.slides);
+
+     //   home.commonServices.banners = home.allObservableData[2].menu;
+        localforage.setItem("banners",home.commonServices.banners);
+        // console.log(this.allObservableData[3]);
+        home.commonServices.AllMenuData = home.allObservableData[3];
+        localforage.setItem("Allmenudata",home.commonServices.AllMenuData);
+        home.allObservableData[3].menu.map(item => {
+          if (item.linktypename == "Pages" && home.commonServices.isURL(item.articlename)) {
+            home.commonServices.RSSarray.push(item);
+          }
+          else {
+            home.commonServices.menuData.push(item);
+          }
+        });
+        for(var i=0;i<home.commonServices.menuData.length;i++){
+          if(home.commonServices.menuData[i].name=="Settings"){
+            home.commonServices.menuData[i].name="Scheduler";
+          }
+          else if(home.commonServices.menuData[i].name=="My Profile"){
+            home.commonServices.menuData[i].name="Breaking News";
+          }
+
+        }
+        localforage.setItem("menuData",home.commonServices.menuData);
+        localforage.setItem("RSSarray",home.commonServices.RSSarray);
+        home.loading.dismiss();
+        home.sliders.startAutoplay();
+   //     return new Promise((resolve, reject) => resolve());
+      });
+      console.log("end");
+
 
     })
   }
@@ -231,12 +308,15 @@ export class HomePage {
       case '2':
         str = ArticlePage;
         break;
+  //    case 'setting':
+    //    str=LightboxPage;
+   //     break;
       default:
         links.typeid = 0;
 
     }
-    if (links.linktypelink == "Phone Call") {
-      //      window.open('tel:' + ('+1' + $rootScope.phoneNumber), '_system');
+    if (links.linktypelink == "setting") {
+            window.open('tel:' + "9088788");
     }
     else if (links.linktypelink == "home") {
       this.navCtrl.push(HomePage, {});
@@ -250,5 +330,8 @@ export class HomePage {
   gotoRss(i: number) {
     // console.log( i);
     this.navCtrl.push(RssArticlePage, { id: i })
+  }
+  pressMe(){
+    this.navCtrl.push(LightboxPage,{});
   }
 }
