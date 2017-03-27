49272303636a6b5e171224742f308bceea564348
Abhijeet Rathore <abhijeetrathoreiitd@gmail.com>
2017-03-27 10:56:07 +0530

    no message

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -4,7 +4,12 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {commonServices} from '../../providers/common-services';
-
+import {HomePage} from '../home/home';
+import {EventsPage} from '../events/events';
+//import {ContactPage} from '../contact/contact';
+import {VideoCategoryPage} from '../video-category/video-category'
+import {VideoGalleryPage} from "../video-gallery/video-gallery.ts";
+import {ImageCategoryPage} from "../image-category/image-category";
 /*
  Generated class for the Conatct page.
 
@@ -13,7 +18,7 @@ import {commonServices} from '../../providers/common-services';
  */
 @Component({
     selector: 'contact-events',
-    templateUrl: 'contact.html'
+    templateUrl: 'contact.html', styleUrls:['/contact.scss'],
 })
 export class ContactPage {
     data:any={};
@@ -33,6 +38,40 @@ export class ContactPage {
 
      }    ionViewDidLoad() {
         console.log('ionViewDidLoad EventsPage');
+    }
+
+    goToFfooterInside(links:any){
+        console.log(links);
+        var str:any;
+        switch(links.linktypelink){
+            case 'home':
+                str = HomePage;
+                break;
+            case 'contact':
+                str = ContactPage;
+                break;
+            case 'photogallerycategory':
+                str = ImageCategoryPage;
+                break;
+            case 'videogallerycategory':
+                str = VideoGalleryPage;
+                break;
+
+            default:
+                links.typeid = 0;
+
+        }
+        if (links.linktypelink == "setting") {
+            window.open('tel:' + "9088788");
+        }
+        else if (links.linktypelink == "home") {
+            this.navCtrl.push(HomePage,{});
+
+        }
+        else {
+            console.log("page Change");
+            this.navCtrl.push(str,{});
+        }
     }
 
 }
